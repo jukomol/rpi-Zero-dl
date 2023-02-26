0bf16fa75be1020291954901508939fd369ba6d7
JU KOMOL <komolma37@gmail.com>
2023-02-26 20:04:37 +0600

    Update tflite_model_run.py

--- Model_Run/tflite_model_run.py
+++ Model_Run/tflite_model_run.py
@@ -2,8 +2,8 @@
 import tensorflow as tf
 import numpy as np
 import cv2 as cv
-import os 
-
+from keras.preprocessing.image import img_to_array
+import os
 
 def img_process(img):
   img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -18,33 +18,37 @@ def img_process(img):
   return img
 
 # Load the TFLite model and allocate tensors.
-interpreter = tf.lite.Interpreter(model_path="D:/Satellite/tflite_quant_model_2.tflite")
+interpreter = tf.lite.Interpreter(model_path="D:/Satellite/tflite_model_2.tflite")
 interpreter.allocate_tensors()
 
 # Get input and output tensors.
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
-dir_path = '/test/bad/0.jpg'
+dir_path = 'D:/Satellite/Lasset_zip/test/bad/'
 count=0
 for i in os.listdir(dir_path):
     img = cv.imread(dir_path+'//' + i)
+    
     img = img_process(img)
-
+    
     X = img_to_array(img)
     X = np.expand_dims(X, axis = 0)
     images = np.vstack([X])
+    #print(images)
     # Test the model on random input data.
     input_shape = input_details[0]['shape']
     input_data = images
     interpreter.set_tensor(input_details[0]['index'], input_data)
 
-    interpreter.invoke()
-    
+    # t1 = time.time()
+    # interpreter.invoke()
+    # t2 = time.time()
     # The function `get_tensor()` returns a copy of the tensor data.
     # Use `tensor()` in order to get a pointer to the tensor.
     output_data = interpreter.get_tensor(output_details[0]['index'])
     print(output_data)
+    # print((t2-t1)*1000)
 
 #Gap
 
